75a22f71a788de317db64bc2109ab6e76d1daaab
Ayham Aljarrah <108863344+AyhamJar@users.noreply.github.com>
2022-11-27 19:46:29 +0400

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -57,8 +57,6 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
-
-
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -76,9 +74,6 @@ def list_accounts():
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 
-
-
-
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
@@ -95,8 +90,6 @@ def get_accounts(account_id):
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
     return account.serialize(), status.HTTP_200_OK
-
-
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
@@ -118,9 +111,6 @@ def update_accounts(account_id):
 
     return account.serialize(), status.HTTP_200_OK
 
-
-
-
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
@@ -137,7 +127,6 @@ def delete_accounts(account_id):
         account.delete()
 
     return "", status.HTTP_204_NO_CONTENT
-
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
